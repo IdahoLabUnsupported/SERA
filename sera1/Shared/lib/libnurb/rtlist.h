27fbc5ea628c55270279e1987ee2cef188772a97
Joshua J. Cogliati <joshua.cogliati@inl.gov>
2024-01-21 12:31:09 -0500

    Copying files from brlcad 35415a02da5e052cf21f0a927284a7374008a886

---
+++ sera1/Shared/lib/libnurb/rtlist.h
@@ -0,0 +1,40 @@
+/*                        R T L I S T . H
+ * BRL-CAD
+ *
+ * Copyright (c) 2004-2007 United States Government as represented by
+ * the U.S. Army Research Laboratory.
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public License
+ * version 2.1 as published by the Free Software Foundation.
+ *
+ * This library is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this file; see the file named COPYING for more
+ * information.
+ */
+/** @addtogroup deprecated */
+/** @{ */
+/** @file rtlist.h
+ * @deprecated
+ *  RTLIST.H has been absorbed into BU.H
+ *  The data tye is now "struct bu_list".
+ *  Compatibility macros are provided in "compat4.h"
+ *	-Mike
+ */
+#include "compat4.h"
+/** @} */
+/*
+ * Local Variables:
+ * mode: C
+ * tab-width: 8
+ * c-basic-offset: 4
+ * indent-tabs-mode: t
+ * End:
+ * ex: shiftwidth=4 tabstop=8
+ */
+
